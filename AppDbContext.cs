394ad81210b2759af689d8eae24fb0c85b75d25d
makram-sys <mzhar711@gmail.com>
2025-08-20 18:48:59 +0300

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Microsoft.EntityFrameworkCore.Sqlite;
+using miniprojectfinal.Models;
+
+namespace miniprojectfinal.DataServices
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        public AppDbContext()
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                
+                optionsBuilder.UseSqlite("Data Source=miniprojectfinal.db");
+               
+            }
+        }
+    }
+}
